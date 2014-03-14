842fdff705fd6016d6968b1332890d6be8f849dc
jimbog <jimmygarzon@gmail.com>
2014-03-13 20:28:12 -0400

    add terminal program

--- bin/csv_terminal.rb
+++ bin/csv_terminal.rb
@@ -1,29 +1,21 @@
 require 'terminal-table'
 require 'csv'
 require_relative '../lib/csv_extras'
+require_relative '../lib/hashes_array'
 
-#get input file
-puts "Please enter the file name(s) separated by spaces, or type default"
+#get input file(s)
+puts "Please enter the file name(s) separated by spaces, or just hit the enter key to load the default files"
 
 file_paths = gets.chomp.downcase
 
-if file_paths == 'default' or ''
+if file_paths == ''
+  #load default files
   file_paths = "people_comma.csv people_pipe.csv people_space.csv"
 end
 
-
-  def combine_csv_files_into_hashes(file_paths)
-     file_paths = file_paths.split(' ')
-    arr = []
-    file_paths.each do |file_path|
-      file = CSV.open File.expand_path("../#{file_path}", __FILE__)
-
-      arr << file.to_hashes
-    end 
-    arr.flatten
-  end
-
-myArr = combine_csv_files_into_hashes(file_paths)
+file_paths_arr = file_paths.split(' ').each do |file_path|
+  File.expand_path("../#{file_path}", __FILE__)
+end
 
 def print_pretty_table(arr_of_hashes)
   arr = []
@@ -31,36 +23,22 @@ def print_pretty_table(arr_of_hashes)
   Terminal::Table.new headings: arr_of_hashes[0].keys, rows: arr
 end
 
-puts print_pretty_table myArr
+myArr = HashesArray.new file_paths_arr
 
-def sort_table(arr, *params)
-  arr.sort_by do |el|
-    params.map { |param| el[param] }
-  end
-end
-
-def column_to_date(hashes_array, column_name, format)
-  hashes_array.each do |row|
-    row[column_name] = Date.strptime(row[column_name],format)
-    end
-end
-
-def format_date(hashes_array, column_name, format)
-  hashes_array.each do |row|
-    row[column_name] = row[column_name].strftime format
-    end
-end
-
-column_to_date(myArr, "DateOfBirth", '%m/%d/%Y')
-
+puts print_pretty_table myArr.arr
 
 puts "Output 1"
-puts print_pretty_table sort_table(myArr, "Gender", "LastName")
+#the HashesArray methods take either strings or symbols
+output1 = myArr.sort_by :gender, :lastname
+puts print_pretty_table output1 
 
-puts "Output 1"
-output2 = sort_table(myArr, "DateOfBirth")
-puts print_pretty_table format_date(output2, "DateOfBirth", '%m/%d/%Y')
-
+puts "Output 2"
+myArr.column_to_date "DateOfBirth", "%m/%d/%Y"
+myArr.sort_by! "DateOfBirth"
+myArr.format_date "DateOfBirth", "%m/%d/%Y"
+puts print_pretty_table myArr.arr 
 
 puts "Output 3"
-puts print_pretty_table sort_table(myArr, "LastName")
+output3 = myArr.sort_by "LastName"
+puts print_pretty_table output3 
+
