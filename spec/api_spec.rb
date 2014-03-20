2d4c19d33ca2499315a37b812116080751b7b270
jimbog <jimmygarzon@gmail.com>
2014-03-20 16:21:58 -0400

    add test for post

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -2,20 +2,25 @@ require 'spec_helper'
 require 'rack/test'
 require_relative '../bin/api'
 
-#describe Grape::API do
-  include Rack::Test::Methods
+include Rack::Test::Methods
 
-  def app
-    CsvApi
-  end
+def app
+  CsvApi
+end
 
-  describe 'Grape::API' do
-    describe "GET /v1/records/lastname" do
-      it "returns an empty array of statuses" do
-        get "/v1/records/lastname"
-        last_response.status.should == 200
-        expect(last_response.body).to include("lastname")
-      end
+describe 'Grape::API' do
+  describe "GET /v1/records/lastname" do
+    it "returns an empty array of statuses" do
+      get "/v1/records/lastname"
+      last_response.status.should == 200
+      expect(last_response.body).to include("lastname")
     end
   end
-#end
+
+  describe "POST /v1/records/newstuff" do
+    it "returns an empty array of statuses" do
+      post "/v1/records/newstuff"
+      last_response.status.should == 201
+    end
+  end
+end
