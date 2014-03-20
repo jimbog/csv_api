822f6327e14510c551c34db7ed0a18c42ad1d58f
jimbog <jimmygarzon@gmail.com>
2014-03-20 16:37:57 -0400

    update readme

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -10,7 +10,7 @@ end
 
 describe 'Grape::API' do
   describe "GET /v1/records/lastname" do
-    it "returns an empty array of statuses" do
+    it "returns an object with data" do
       get "/v1/records/lastname"
       last_response.status.should == 200
       expect(last_response.body).to include("lastname")
@@ -18,7 +18,7 @@ describe 'Grape::API' do
   end
 
   describe "POST /v1/records/newstuff" do
-    it "returns an empty array of statuses" do
+    it "receives and processes post requests" do
       post "/v1/records/newstuff"
       last_response.status.should == 201
     end
